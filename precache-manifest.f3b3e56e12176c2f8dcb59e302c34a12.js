7676549879188829b1e35c54f1541e654db75147
Anna Ikonomova <ani.strandjeva@gmail.com>
2019-06-21 17:56:45 +0300

    Updates

---
+++ precache-manifest.f3b3e56e12176c2f8dcb59e302c34a12.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dab03c5354372d0006571872b5c58d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26fbbede03c8d9138fd5",
+    "url": "/static/css/main.193fc165.chunk.css"
+  },
+  {
+    "revision": "ae819a341f5ea6c13fb0",
+    "url": "/static/js/2.33728bfe.chunk.js"
+  },
+  {
+    "revision": "26fbbede03c8d9138fd5",
+    "url": "/static/js/main.47344713.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "decfd73e72004102763c1f109bb5c1f5",
+    "url": "/static/media/1.decfd73e.jpg"
+  },
+  {
+    "revision": "7ee148c4d5929ce9e1d416add75c6936",
+    "url": "/static/media/20.7ee148c4.jpg"
+  },
+  {
+    "revision": "fc9310ce66096dc57ba6ac9b240d1c0f",
+    "url": "/static/media/2322.fc9310ce.jpg"
+  },
+  {
+    "revision": "1908a1403fec2df70c79045b0a7cd01c",
+    "url": "/static/media/matchingGame.1908a140.jpg"
+  },
+  {
+    "revision": "5f462979525db25e5afa09f948c5bfc8",
+    "url": "/static/media/myReads.5f462979.jpg"
+  },
+  {
+    "revision": "3996d633409f47eb3ac224b844dff305",
+    "url": "/static/media/pam.3996d633.jpg"
+  },
+  {
+    "revision": "4db8f54fb3a56c4575b1586598856582",
+    "url": "/static/media/plovdiv.4db8f54f.jpg"
+  },
+  {
+    "revision": "d3c7ea15b102ad45d04d6c50157028ed",
+    "url": "/static/media/snail.d3c7ea15.jpg"
+  },
+  {
+    "revision": "14d82c28bd23704ac3e87d5bd6949f8f",
+    "url": "/static/media/theRunner.14d82c28.jpg"
+  }
+]);
