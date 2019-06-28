9dbcc51332289a39a7ce864da9de2ed1d2dcdfeb
Anna Ikonomova <ani.strandjeva@gmail.com>
2019-06-28 18:02:32 +0300

    Fix padding

--- src/Navigation.js
+++ src/Navigation.js
@@ -11,8 +11,8 @@ class Navigation extends Component {
     <div id= 'menu'>
     <Link to="/" aria-label="Home-page" tabIndex="0"><img src={logo} className="logo" alt="logo" /></Link>
     <ul className='nav' tabIndex='-1'>
-    <Link to="/"><li className='navigation'><i className="fa fa-home w3-xxlarge"></i><span>Home</span></li></Link>
-    <Link to="/my-work"><li className='navigation'><i className="fa fa-eye w3-xxlarge"></i><span>Work</span></li></Link>
+    <li className='navigation'><Link to="/"><i className="fa fa-home w3-xxlarge"></i>Home</Link></li>
+    <li className='navigation'><Link to="/my-work"><i className="fa fa-eye w3-xxlarge"></i>Work</Link></li>
     </ul>
     </div>
     );
