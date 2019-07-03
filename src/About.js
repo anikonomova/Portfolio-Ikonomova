083a289d89a1e7bf63664b56dc947b72c2225f04
Anna Ikonomova <ani.strandjeva@gmail.com>
2019-07-03 22:59:46 +0300

    Fix about.js

--- src/About.js
+++ src/About.js
@@ -15,8 +15,8 @@ class About extends Component {
       </div>
       <div className='box1'><h3 className='title2'>About me</h3>
       <p className='about'>My Name is Anna Ikonomova and I live in Gummersbach, Germany. I am Junior Front-End Web Developer.
-      The first line of code, that I wrote, was when the Google Udacity Scolarship Challenge
-      course began at the end of 2017. After 3 months I won the Scolarship and I had the chance to study and graduate the Nanodegree program
+      The first line of code, that I wrote, was when the Google Udacity Scholarship Challenge
+      course began at the end of 2017. After 3 months I won the scholarship and I had the chance to study and graduate the Nanodegree program
       for Front-End Web Developers.
       Now I'm still learning and willing to build a career in the field.
       </p>
